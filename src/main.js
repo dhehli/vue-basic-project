09587883f054a57bfde1f86d79edae85fc7d3a27
dhehli <dh@netlive.ch>
2019-07-18 18:31:18 +0200

    move image folder

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
-import './assets/scss/main.scss';
+import '@/assets/scss/main.scss';
 
 Vue.use(BootstrapVue)
 
