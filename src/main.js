c4067ecaca2e39ae0041d34bcf5bcc3c8ead35ea
dhehli <dh@netlive.ch>
2019-07-18 18:29:36 +0200

    add custom bootstrap scss

--- src/main.js
+++ src/main.js
@@ -7,6 +7,8 @@ import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+import './assets/scss/main.scss';
+
 Vue.use(BootstrapVue)
 
 Vue.config.productionTip = false
