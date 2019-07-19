10d9cb078f3f5f13fb98d685ff3c49026d5ff1ad
dhehli <dh@netlive.ch>
2019-07-19 09:12:15 +0200

    set active class

--- src/components/layout/MainNav.vue
+++ src/components/layout/MainNav.vue
@@ -6,7 +6,7 @@
 
     <b-collapse id="nav-collapse" is-nav>
       <b-navbar-nav>
-          <b-nav-item to="/">Home</b-nav-item>
+          <b-nav-item to="/" exact>Home</b-nav-item>
           <b-nav-item to="/animal">Animals</b-nav-item>
       </b-navbar-nav>
 
